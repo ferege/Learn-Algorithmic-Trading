1dbdfb106c741cfae1e0eaadc81fe13edd400499
ferege <39501109+ferege@users.noreply.github.com>
2020-05-09 03:11:39 +0300

    Update apo.py

--- Chapter2/apo.py
+++ Chapter2/apo.py
@@ -13,6 +13,17 @@ except FileNotFoundError:
   goog_data2.to_pickle(SRC_DATA_FILENAME)
 
 goog_data = goog_data2.tail(620)
+lows=goog_data['Low']
+highs=goog_data['High']
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel='Google price in $')
+highs.plot(ax=ax1, color='c', lw=2.)
+lows.plot(ax=ax1, color='y', lw=2.)plt.
+hlines(highs.head(200).max(),lows.index.values[0],lows.index.values[-1],linewidth=2, color='g')plt.
+hlines(lows.head(200).min(),lows.index.values[0],lows.index.values[-1],linewidth=2, color='r')plt.
+axvline(linewidth=2,color='b',x=lows.index.values[200],linestyle=':')
+plt.show()
 
 close = goog_data['Close']
 
